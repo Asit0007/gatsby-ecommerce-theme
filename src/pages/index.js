b78ce95d04eb97cbeb751ccbfc4a1aa550179eaa
Asit <86393824+Asit0007@users.noreply.github.com>
2024-09-09 13:48:33 +0530

    Update index.js
    removed messages

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,16 +38,7 @@ const IndexPage = () => {
       />
 
       {/* Message Container */}
-      <div className={styles.messageContainer}>
-        <p>
-          This is a demonstration of the Sydney theme for verse by{' '}
-          <span className={styles.gold}>matter design.</span>
-        </p>
-        <p>
-          wear by <span className={styles.gold}>sunspel</span> and{' '}
-          <span className={styles.gold}>scotch&soda</span>
-        </p>
-      </div>
+      
 
       {/* Collection Container */}
       <div className={styles.collectionContainer}>
@@ -99,7 +90,7 @@ const IndexPage = () => {
       {/* Quote */}
       <Quote
         bgColor={'var(--standard-light-grey)'}
-        title={'about Sydney'}
+        title={'about BFAF'}
         quote={
           '“We believe in two things: the pursuit of quality in everything we do, and looking after one another. Everything else should take care of itself.”'
         }
@@ -130,8 +121,8 @@ const IndexPage = () => {
       {/* Social Media */}
       <div className={styles.socialContainer}>
         <Title
-          name={'Styled by You'}
-          subtitle={'Tag @sydney to be featured.'}
+          name={'Styled by Asit'}
+          subtitle={'Tag @asit to be featured.'}
         />
         <div className={styles.socialContentGrid}>
           <img src={toOptimizedImage(`/social/socialMedia1.png`)} alt={'social media 1'} />
